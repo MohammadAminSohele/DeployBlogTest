78fc9754f5fd4df1153ee37783a3b440ba571517
MohammadAminSohaili <122188222+MohammadAminSohele@users.noreply.github.com>
2024-08-24 06:44:33 +0330

    Create Article Models

--- Article/models.py
+++ Article/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
+from django.utils import timezone
+
 # Create your models here.
+
+class Article(models.Model):
+    STATUS_CHOICES=(
+        ('d','Draft'),
+        ('p','Published'),
+    )
+
+    title=models.CharField(max_length=200,verbose_name='عنوان')
+    slug=models.SlugField(max_length=100,verbose_name='عنوان در url')
+    description=models.TextField(verbose_name='توضیحات')
+    images=models.ImageField(upload_to='images',verbose_name='عکس')
+    published=models.DateTimeField(default=timezone.now,verbose_name='تاریخ انتشار')
+    created=models.DateTimeField(auto_now_add=True,verbose_name='تاریخ ساخت')
+    updated=models.DateTimeField(auto_now=True,verbose_name='تاریخ اپدیت')
+    status=models.CharField(max_length=1,choices=STATUS_CHOICES,verbose_name='حالت')
+
+    def __str__(self):
+        return self.title
