b9cabcbdb67493763408e1be291533d76aad0b54
MohammadAminSohaili <122188222+MohammadAminSohele@users.noreply.github.com>
2024-08-24 06:45:56 +0330

    Update Static Settings

--- config/urls.py
+++ config/urls.py
@@ -17,7 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 
-from django.conf import settings
+from .import settings
 from django.conf.urls.static import static
 
 from .import views
@@ -34,5 +34,6 @@ urlpatterns = [
     path('Article/',include('Article.urls')),
 ]
 
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
