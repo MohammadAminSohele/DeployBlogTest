3f21cda8daae37a48a6a37767bff43925c90cd5e
MohammadAminSohaili <122188222+MohammadAminSohele@users.noreply.github.com>
2024-08-31 10:28:13 +0330

    get_object_or_404 add to article_detail

--- Article/views.py
+++ Article/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 
 from .models import Article
 
@@ -6,6 +6,6 @@ from .models import Article
 
 def article_detail(request,slug):
     context={
-        'article':Article.objects.get(slug=slug)
+        'article':get_object_or_404(Article,slug=slug,status='p')
     }
     return render(request,'detail.html',context)
