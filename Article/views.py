1971223f5b2170eb78158f0e5f58c79b8a1687f1
MohammadAminSohele <mohammadaminsohele@gmail.com>
2024-10-04 19:13:07 +0330

    Update

--- Article/views.py
+++ Article/views.py
@@ -1,29 +1,13 @@
 from django.shortcuts import render,get_object_or_404
 
-from Article.models import Article,Catagory
+from Article.models import Article
 
 # Create your views here.
 
 def home(request):
-    Slider=Article.objects.order_by('-created').all()[:1]
     context={
-        # region ValueList
-        # 'articles':[
-        #     {
-        #         'title':'title1',
-        #         'description':'description 1'
-        #     },
-        #      {
-        #         'title':'title2',
-        #         'description':'description 2'
-        #     }
-        # ]
-        # endregion
-
-        'title':'Home page',
-        'Slider':Slider,
+        'Slider':Article.objects.order_by('-created').all()[:1],
         'Article':Article.objects.filter(status='p').order_by('created'),
-        # 'Catagory':Catagory.objects.filter(status=True)
     }
     return render(request,'home.html',context)
 
